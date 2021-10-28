e76f76c18450d52f75f39275344e82c6b147790f
Jorge Salmon <kokesalmon@gmail.com>
2021-10-28 17:19:23 +0200

    commit cambio de nombre

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Teorema-3x-1
+# Teorema 3x+1
 Práctica de hilos.
 
 Consiste en crear un analizador de casos para el teorema 3x-1.
--- Teorema 3x+1/src/teorema/pkg3x/pkg1/Principal.java
+++ Teorema 3x+1/src/teorema/pkg3x/pkg1/Principal.java
@@ -2,6 +2,6 @@ package teorema.pkg3x.pkg1;
 
 public class Principal {
     public static void main(String[] args) {
-        //hola puto haver si esto sale bn 
+        
     }// main()
 }// Principal
