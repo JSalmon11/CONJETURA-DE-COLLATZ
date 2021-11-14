94a2b41de0738527d6d9dc8c6a2422235383b3c5
Jorge Salmon <kokesalmon@gmail.com>
2021-11-14 11:38:05 +0100

    commit reduciendo comentarios

--- Teorema 3x+1/src/teorema/pkg3x/pkg1/HiloRangos.java
+++ Teorema 3x+1/src/teorema/pkg3x/pkg1/HiloRangos.java
@@ -12,9 +12,7 @@ public class HiloRangos implements Runnable {
     private BigInteger DOS=new BigInteger("2");
     private BigInteger TRES=new BigInteger("3");
     //Con este HashMap podre comprobar si el resultado de una operacion se
-    //esta repitiendo y asi el programa esta preparado para encontrar un
-    //bucle diferente al 4,2,1 ademas de salir del while que
-    //hace las operacipones cuando llega a ese u otro bucle
+    //esta repitiendo y asi el programa detecta bucles
     private HashMap <BigInteger, BigInteger> repetido = new HashMap ();
     //String para mandar la secuencia completa de un numero tras haberla calculado
     private String secuencia="";
@@ -36,13 +34,11 @@ public class HiloRangos implements Runnable {
             resultado=i;
             //creo un contador para saber la secuencia mas larga
             int contSecuencias=0;
-            //Mientras el value del numero que se va a calcular es nulo
-            //se hacen los calculos, cuando el value no es nulo significa
-            //que se ha entrado en un bucle, el 4,2,1 u otro distinto
+            //Mientras el value del numero que se va a calcular es nulo los calculos,
+            //cuando el value no es nulo significa que se ha entrado en un bucle
             while (repetido.get(resultado) == null) {
                 numActual=resultado;
-                //Compruebo desde el hashmap de datos si
-                //este numero ya esta calculado o no
+                //Compruebo desde el hashmap de datos si este numero ya esta calculado
                 if (datos.calculados(numActual)==false) {
                     if (numActual.remainder(DOS)==BigInteger.ZERO){
                         resultado=numActual.divide(DOS);
@@ -57,22 +53,19 @@ public class HiloRangos implements Runnable {
                     //Si el numero ya se ha calculado, no hago mas operaciones
                     //y le pido a datos el resto de la longitud de la secuencia
                     contSecuencias+=datos.getSecuencia(resultado);
-                    //Dado que esta secuencia ya ha sido explorada, ya se ha
-                    //comprobado si es el bucle 4,2,1 o es un nuevo bucle la
-                    //primera vez que se calculo la secuencia,
-                    //asi que dejo la variable a false 
+                    //Dado que esta secuencia ya ha sido explorada, ya se ha comprobado si
+                    //es el bucle 4,2,1 o es un nuevo bucle la primera vez que se calculo
+                    //la secuencia, asi que dejo la variable a false 
                     posibleNuevoBucle=false;
                 }
                 secuencia+=numActual+"-";
                 //Se va rellenando el hashmap que comprueba el primer numero que se repite y por tanto el bucle
                 repetido.put(numActual, resultado);
-                //Comparo el numero alcanzado para ver si es el maximo numero alcanzado
                 if (resultado.compareTo(datos.getNumMax())==1){
                     datos.setNumMax(resultado);
                 }    
             }
-            //Si la variable esta en true, significa que esa secuencia no
-            //se ha comprobado si desemboca en un nuevo bucle
+            //Si la variable esta en true, significa que esa secuencia no se ha comprobado si desemboca en un nuevo bucle
             if (posibleNuevoBucle){
                 comprobarPosibleNuevoBucle(numActual, i);
             }
@@ -80,14 +73,14 @@ public class HiloRangos implements Runnable {
             //Manda la secuencia completa del numero, o si llego a un numero ya calculado solo
             //se manda la parte que aun no ha sido calculada para actualizar el hashmap de calculados
             datos.actualizarCalculados(secuencia);
-            //Manda el contador de la secuencia junto con la semilla que provoca esa secuencia
+            //Manda el contador de la secuencia junto con la semilla que provoca esa secuencia para comprobar si es la mas larga
             datos.secuenciaMasLarga(i, contSecuencias);
             repetido.clear();
         }
     }// run()
     
     private void comprobarPosibleNuevoBucle(BigInteger numRepetido, BigInteger semilla) {
-        //Si el numero que se repite no es el 4 o el 2 o el 1 y no es el 0, para
+        //Si el numero que se repite no es el 4 ni el 2 ni el 1 y no es el 0, para
         //controlar errores, significa que se ha encontrado un bucle nuevo
         if (!numRepetido.equals(BigInteger.ZERO)
         && !repetido.get(numRepetido).equals(DOS.add(DOS))
